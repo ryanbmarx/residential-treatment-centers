c1b629e94d72c7c9c218ff3ea254bd259244909e
Ryan Marx <rymarx@tribune.com>
2014-12-02 12:24:18 -0600

    new prod url

--- tarbell_config.py
+++ tarbell_config.py
@@ -24,7 +24,7 @@ S3_BUCKETS = {
     # Provide target -> s3 url pairs, such as:
     #     "mytarget": "mys3url.bucket.url/some/path"
     # then use tarbell publish mytarget to publish to it
-    "production": "graphics.chicagotribune.com/news/watchdog/rtc",
+    "production": "graphics.chicagotribune.com/news/watchdog/rtc/graphic-essays",
     "staging": "graphics.beta.tribapps.com/rtc-graphic-novels",
     "kaarin": "graphics.chicagotribune.com/rtc-graphic-novels-for-kaarin-and-only-for-her-eyes-dfhjskfhsd-dfhglkdfhgkjdf"
 }
